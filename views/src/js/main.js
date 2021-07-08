29dabc153092a3ed9e096a73b59a02dc9e3787f9
anhyeon <anhye0ndev@gmail.com>
2021-07-09 07:15:26 +0900

    default setting

--- views/src/js/main.js
+++ views/src/js/main.js
@@ -1,23 +1,25 @@
 let back_image = document.getElementsByClassName('best_site_img')
 
 function img_change() {
-    if (back_image[0].src === 'http://localhost:63342/web/vanilla/%EC%87%BC%ED%95%91%EB%AA%B0/src/img/b2.jpg') {
-        for (i = 0; i < back_image.length; i++) {
+    for (i = 0; i < 40; i++) {
+        if (back_image[i].src === 'http://anhye0n.me/shop/src/img/b2.jpg') {
             back_image[i].src = 'src/img/b1.jpg'
-        }
-    } else if (back_image[0].src === 'http://localhost:63342/web/vanilla/%EC%87%BC%ED%95%91%EB%AA%B0/src/img/b1.jpg') {
-        for (i = 0; i < back_image.length; i++) {
+        } else if (back_image[i].src === 'http://anhye0n.me/shop/src/img/b1.jpg') {
             back_image[i].src = 'src/img/b2.jpg'
         }
     }
-
+    // if (back_image[0].src === 'http://anhye0n.me/shop/src/img/b2.jpg') {
+    //     for (i = 0; i < back_image.length; i++) {
+    //         back_image[i].src = 'src/img/b1.jpg'
+    //     }
+    // } else if (back_image[0].src === 'http://anhye0n.me/shop/src/img/b1.jpg') {
+    //     for (i = 0; i < back_image.length; i++) {
+    //         back_image[i].src = 'src/img/b2.jpg'
+    //     }
+    // }
 }
 
-function img_init() {
-    setInterval(img_change, 1000)
-}
-
-img_init()
+setInterval(img_change, 1000)
 
 let best_site_li = document.getElementsByClassName('best_site_li')
 let best_site_li_length = document.getElementsByClassName('best_site_li').length
@@ -26,10 +28,11 @@ let best_site_text = document.getElementsByClassName('best_site_text')
 let best_site_text_length = document.getElementsByClassName('best_site_text').length
 
 best_count = 0
-if (best_count === 0){
+if (best_count === 0) {
     best_site_text[best_count].style.background = '#f5f5f5'
 }
-function best_site_next(){
+
+function best_site_next() {
     if (best_count > best_site_li_length - 1) {
         best_count = 0
     }
@@ -48,7 +51,7 @@ function best_site_next(){
 
 }
 
-function best_site_move(real_num){
+function best_site_move(real_num) {
     best_count = real_num
 
     for (i = 0; i < best_site_li_length; i++) {
